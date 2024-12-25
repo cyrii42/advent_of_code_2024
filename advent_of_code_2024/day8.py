3687b19b1a489cbedec4dc8c8c8626151a020b56
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-12-25 00:50:20 -0500

    more tinkering with day 8...

--- advent_of_code_2024/day8.py
+++ advent_of_code_2024/day8.py
@@ -84,11 +84,13 @@ Calculate the impact of the signal. How many unique locations within the bounds 
 
 from pathlib import Path
 from rich import print
+from pprint import pprint
 from copy import deepcopy
 from typing import NamedTuple, Protocol
 from enum import Enum
 from dataclasses import dataclass
 from string import ascii_letters
+import itertools
 import pandas as pd
 import numpy as np
 from alive_progress import alive_it
@@ -97,42 +99,35 @@ from advent_of_code_2024.constants import DATA_DIR
 EXAMPLE = DATA_DIR / 'day8_example.txt'
 INPUT = DATA_DIR / 'day8_input.txt'
 
-
-@dataclass
-class MapRow():
-    string: str
+@dataclass(frozen=True)
+class Antenna():
+    char: str
     row_num: int
-
-    @property
-    def width(self) -> int:
-        return len(self.string)
-
-    def get_point(self, col_num: int) -> str:
-        return self.string[col_num]
-
-@dataclass
-class MapColumn():
-    string: str
     col_num: int
 
     @property
-    def height(self) -> int:
-        return len(self.string)
+    def vector(self) -> np.ndarray:
+        return np.array([self.row_num, self.col_num])
+        
 
-    def get_point(self, row_num: int) -> str:
-        return self.string[row_num]
+@dataclass(frozen=True)
+class Antinode():
+    char: str
+    row_num: int
+    col_num: int
 
-@dataclass
-class MapDiagonal():
-    string: str
+    def validate(self, map_height: int) -> bool:
+        return self.row_num >= 0 and self.row_num < map_height and self.col_num >= 0 and self.col_num < map_height
 
-    def get_point(self, num: int) -> str:
-        return self.string[num]
+    def __eq__(self, other) -> bool:
+        return self.row_num == other.row_num and self.col_num == other.col_num
 
+    def __hash__(self):
+        return hash((self.row_num, self.col_num))
 
 @dataclass
 class Map():
-    row_list: list[MapRow]
+    row_list: list[str]
 
     @property
     def height(self) -> int:
@@ -140,90 +135,102 @@ class Map():
 
     @property
     def width(self) -> int:
-        return self.row_list[0].width
+        return len(self.row_list)
 
     @property
-    def col_list(self) -> list[MapColumn]:
-        output_list = []
-        for i, row in enumerate(self.row_list):
-            output_list.append(MapColumn(
-                                string=''.join(row.string[i] for row in self.row_list),
-                                col_num=i
-                            ))
-        return output_list
+    def string(self) -> str:
+        return ''.join(row for row in self.row_list)
 
     @property
-    def diagonal_list(self) -> list[MapDiagonal]:
-        num_columns = self.width
-        num_rows = self.height 
+    def antenna_types(self) -> set[str]:
+        return set([char for char in self.string if char.isalnum()])
 
-        row_strings = [list(row.string) for row in self.row_list]
-
+    @property
+    def antenna_list(self) -> list[Antenna]:
         output_list = []
-        
-        df = pd.DataFrame(row_strings)
-        for x in range(0-num_rows, num_columns-1):
-            output_list.append(MapDiagonal(string=''.join(x for x in np.diag(df, k=x))))
-
-        df2 = df.iloc[::-1].reset_index(drop=True)
-        for x in range(0-num_rows, num_columns-1):
-            output_list.append(MapDiagonal(string=''.join(x for x in np.diag(df, k=x))))
-
+        for row_num, row in enumerate(self.row_list):
+            for col_num, char in enumerate(row):
+                if char.isalnum():
+                    output_list.append(Antenna(char, row_num, col_num))
         return output_list
-        
-    def get_row(self, row_num: int) -> MapRow:
-        return self.row_list[row_num]
 
-    def get_column(self, col_num: int) -> MapColumn:
-        return self.col_list[col_num]
-
-    def get_point(self, row_num: int, col_num: int) -> str:
-        row = self.row_list[row_num]
-        return row.get_point(col_num)
-
-    def find_antinodes(self):
+    def print(self):
         for row in self.row_list:
-            antennas = [char for char in row.string if char.isalnum()]
-            repeated_antennas = [char for char in antennas if antennas.count(char) > 1]
-            print(repeated_antennas)
+            print(row)
 
-        for col in self.col_list:
-            antennas = [char for char in col.string if char.isalnum()]
-            repeated_antennas = [char for char in antennas if antennas.count(char) > 1]
-            print(repeated_antennas)
+    def find_antinodes(self) -> set[Antinode]:
+        output_list: list[Antinode] = []
+        for char in self.antenna_types:
+            antennas = [x for x in self.antenna_list if x.char == char]
+            combos = itertools.combinations(antennas, 2)
+            for combo in combos:
+                antenna_1 = combo[0]
+                antenna_2 = combo[1]
+                # print(f"{antenna_1} - {antenna_2}")
+                output_list += self.calculate_antinodes(antenna_1, antenna_2)
+        return set([antinode for antinode in output_list if antinode.validate(map_height=self.height)])
 
-        for diag in self.diagonal_list:
-            antennas = [char for char in diag.string if char.isalnum()]
-            repeated_antennas = [char for char in antennas if antennas.count(char) > 1]
-            print(repeated_antennas)
+    def calculate_antinodes(self, antenna_1: Antenna, antenna_2: Antenna) -> tuple[Antinode, Antinode]:                
+        row_diff = antenna_1.row_num - antenna_2.row_num
+        col_diff = antenna_1.col_num - antenna_2.col_num
+        
+        antinode_1 = Antinode(char=antenna_1.char,
+                              row_num=antenna_1.row_num - row_diff,
+                              col_num=antenna_1.col_num - col_diff)
 
-def create_map_row(line: str, row_num: int) -> MapRow:
-    return MapRow(line, row_num)
+        antinode_2 = Antinode(char=antenna_2.char,
+                              row_num=antenna_2.row_num + row_diff,
+                              col_num=antenna_2.col_num + col_diff)
+
+        return (antinode_1, antinode_2)
+            
 
 def create_map(filename: Path) -> Map:
     with open(filename, 'r') as f:
         line_list = [line.strip('\n') for line in f.readlines()]
-
-    row_list = [create_map_row(line, row_num) for row_num, line in enumerate(line_list)]
         
-    return Map(row_list)
+    return Map(line_list)
 
-def part_one(filename: Path) -> int:
+def part_one(filename: Path):
     map = create_map(filename)
-    print(map.row_list)
-    print(map.col_list)
-    print(map.diagonal_list)
-    map.find_antinodes()
+    # map.print()
+    # print()
+    # print(map.antenna_list)
+    # print(map.antenna_types)
+    antinodes = map.find_antinodes()
+    # print(map.height)
+    # print(asdf)
+    return len(antinodes)
 
-def part_two(filename: Path) -> int:
+def part_two(filename: Path):
     ...
 
 def main():
-    print(f"Part One (example):  {part_one(EXAMPLE)}")
-    # print(f"Part One (input):  {part_one(INPUT)}")
+    print(f"Part One (example):  {part_one(EXAMPLE)}") # 14
+    print(f"Part One (input):  {part_one(INPUT)}") # 393 is too high; 370 is too high; 360 is too low
     # print()
     # print(f"Part Two (example):  {part_two(EXAMPLE)}")
     # print(f"Part Two (input):  {part_two(INPUT)}")
+
+    # aaa = Antinode('C', 1, 1)
+    # bbb = Antinode('D', 1, 1)
+    # print(aaa == bbb)
+    # c = [aaa, bbb]
+    # print(c)
+    # print(set(c))
+
+    # f = [1, 2, 3]
+    # print(list(itertools.combinations(f, 2)))
+
+    # a = np.array([2, 4])
+    # b = np.array([5, 8])
+    # c = a + b
+    # d = a - b
+    # print(type(c))
+    # print(c)
+    # print(d)
+    
+
 
 if __name__ == '__main__':
     main()
