c792d34782fe4a82d62ba2f3b76097fb50a3f194
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-12-25 23:29:56 -0500

    solved day 11!

--- advent_of_code_2024/day11.py
+++ advent_of_code_2024/day11.py
@@ -74,6 +74,7 @@ from enum import Enum
 from dataclasses import dataclass, field
 from string import ascii_letters
 import itertools
+import functools
 import pandas as pd
 import numpy as np
 from alive_progress import alive_it
@@ -94,8 +95,6 @@ class Stone():
         elif isinstance(num, int):
             self.num_str = str(num)
             self.num_int = num
-        else:
-            raise TypeError(f"Invalid input type: {type(num)} (should be str or int).")
 
     def __repr__(self):
         return f"{self.num_str}"
@@ -103,40 +102,47 @@ class Stone():
 @dataclass
 class StoneLine():
     stones: list[Stone]
+    stone_dict: dict[int, int] = field(default_factory=dict, repr=False)
+
+    def __post_init__(self):
+        self.create_stone_dict()
+
+    def create_stone_dict(self):
+        for stone in self.stones:
+            if stone.num_int in self.stone_dict.keys():
+                self.stone_dict[stone.num_int] += 1
+            else:
+                self.stone_dict[stone.num_int] = 1
 
     def blink(self) -> None:
-        new_stone_list = []
-        for stone in self.stones:
-            new_stone_list += transform_stone(stone)
-        self.stones = new_stone_list
-        # print(self.stones)
+        new_dict: dict[int, int] = {}
+        for num_int, num_occurrences in self.stone_dict.items():
+            new_stones = transform_stone(Stone(num_int))
+            for new_stone in new_stones:
+                if new_stone.num_int in new_dict.keys():
+                    new_dict[new_stone.num_int] += num_occurrences
+                else:
+                    new_dict[new_stone.num_int] = num_occurrences
+        self.stone_dict = new_dict
+
 
     def __repr__(self):
         return f"{self.stones}"
 
-def transform_stone(stone: Stone) -> list[Stone]:
-    '''    
-    - If the stone is engraved with the number 0, it is replaced by a stone 
-    engraved with the number 1.
-    
-    - If the stone is engraved with a number that has an even number of digits, 
-    it is replaced by two stones. The left half of the digits are engraved on the 
-    new left stone, and the right half of the digits are engraved on the new right stone. 
-    (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-    
-    - If none of the other rules apply, the stone is replaced by a new stone; the old 
-    stone's number multiplied by 2024 is engraved on the new stone.'''
-    
+
+def transform_stone(stone: Stone) -> list[Stone]:   
     if stone.num_int == 0:
         return [Stone(1)]
 
     if len(stone.num_str) % 2 == 0:
         middle_idx = len(stone.num_str) // 2
-        stone1 = Stone(int(stone.num_str[0:middle_idx]))
-        stone2 = Stone(int(stone.num_str[middle_idx:]))
-        return [stone1, stone2]
+        int1 = int(stone.num_str[0:middle_idx])
+        int2 = int(stone.num_str[middle_idx:])
+        return [Stone(int1), Stone(int2)]
 
-    return [Stone(stone.num_int * 2024)]
+    else:
+        new_int = stone.num_int * 2024
+        return [Stone(new_int)]
 
 
 
@@ -146,7 +152,7 @@ def part_one(filename: Path):
     stone_line = StoneLine(stone_list)
     for _ in alive_it(range(25)):
         stone_line.blink()
-    return len(stone_line.stones)
+    return sum(stone_line.stone_dict.values())
 
 
 def part_two(filename: Path):
@@ -155,7 +161,7 @@ def part_two(filename: Path):
     stone_line = StoneLine(stone_list)
     for _ in alive_it(range(75)):
         stone_line.blink()
-    return len(stone_line.stones)
+    return sum(stone_line.stone_dict.values())
 
 
 def main():
@@ -163,7 +169,7 @@ def main():
     print(f"Part One (input):  {part_one(INPUT)}") # 213625
     print()
     # print(f"Part Two (example):  {part_two(EXAMPLE)}") # ???
-    print(f"Part Two (input):  {part_two(INPUT)}") #
+    print(f"Part Two (input):  {part_two(INPUT)}") # 252442982856820
 
 
 
